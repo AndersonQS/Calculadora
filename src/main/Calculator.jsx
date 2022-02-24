7f3f18b0057fafc5e68bf46fe929ba96eeb7a791
Hunterand <andersonaracati10@gmail.com>
2022-02-24 16:59:12 -0300

    final

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -27,7 +27,28 @@ export default class Calculator extends Component {
 
   setOperation(operation) {
     if (this.state.current === 0) {
-        this.setState({operation, current: 1, clearDisplay: true})
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try{
+        
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      }catch(e) {
+        values[0] = currentOperation;
+      }
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+
+      });
     }
   }
 
@@ -36,12 +57,11 @@ export default class Calculator extends Component {
       return
     }
 
-    const clearDisplay = this.state.displayValue === '0'
-      || this.state.clearDisplay
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
 
     const currentValue = clearDisplay ? '' : this.state.displayValue;
-
     const displayValue = currentValue + n;
+
     this.setState({ displayValue, clearDisplay: false });
 
     if (n !== '.') {
@@ -50,6 +70,7 @@ export default class Calculator extends Component {
       const values = [...this.state.values];
       values[i] = newValue;
       this.setState({ values });
+      console.log(values);
 
 
     }
@@ -72,7 +93,7 @@ export default class Calculator extends Component {
         <Button label="1" click={this.addDigit} />
         <Button label="2" click={this.addDigit} />
         <Button label="3" click={this.addDigit} />
-        <Button label="+" click={this.addDigit} operation />
+        <Button label="+" click={this.setOperation} operation />
         <Button label="0" click={this.addDigit} double />
         <Button label="." click={this.addDigit} />
         <Button label="=" click={this.setOperation} />
